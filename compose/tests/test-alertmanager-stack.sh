96700d2b34280d9a272257c6493e5eab20c5fe05
imcherry5778 <imcherry5778@gmail.com>
2026-07-13 01:14:54 +0900

    feat: add ScaleCart demo recovery alerts

--- compose/tests/test-alertmanager-stack.sh
+++ compose/tests/test-alertmanager-stack.sh
@@ -25,12 +25,14 @@ alertmanager_config="${REPO_ROOT}/compose/stacks/observability/alertmanager/conf
 prometheus_compose="${REPO_ROOT}/compose/stacks/observability/prometheus/compose.yaml"
 prometheus_config="${REPO_ROOT}/compose/stacks/observability/prometheus/config/prometheus.yml"
 smoke_rules="${REPO_ROOT}/compose/stacks/observability/prometheus/config/alerts/alertmanager-smoke.yml"
+demo_rules="${REPO_ROOT}/compose/stacks/observability/prometheus/config/alerts/demo-scalecart.yml"
 homepage_services="${REPO_ROOT}/compose/stacks/edge/homepage/config/services.yaml"
 vault_agent_config="${REPO_ROOT}/compose/stacks/security/vault-agent/config/agent.hcl"
 
 assert_file "$alertmanager_compose"
 assert_file "$alertmanager_config"
 assert_file "$smoke_rules"
+assert_file "$demo_rules"
 assert_file "$vault_agent_config"
 
 assert_contains "$alertmanager_compose" "image: prom/alertmanager:"
@@ -47,14 +49,24 @@ assert_contains "$alertmanager_config" 'severity="test"'
 assert_contains "$alertmanager_config" 'receiver: "null"'
 assert_contains "$alertmanager_config" "receiver: slack-infra-alerts"
 assert_contains "$alertmanager_config" "repeat_interval: 4h"
+assert_contains "$alertmanager_config" 'demo="true"'
+assert_contains "$alertmanager_config" '대시보드: <{{ .Annotations.dashboard_url }}|Teleport에서 Grafana 열기>'
 
 assert_contains "$prometheus_compose" "./config/alerts:/etc/prometheus/alerts:ro,Z"
+assert_contains "$prometheus_compose" '100.117.59.96:9090:9090'
 assert_contains "$prometheus_config" "alertmanagers:"
 assert_contains "$prometheus_config" "alertmanager:9093"
 assert_contains "$prometheus_config" "/etc/prometheus/alerts/*.yml"
 
 assert_contains "$smoke_rules" "AlertmanagerSmokeTest"
 assert_contains "$smoke_rules" 'vector(0) == 1'
+assert_contains "$demo_rules" "name: demo-scalecart"
+assert_contains "$demo_rules" "alert: ScaleCartWorkerAvailabilityDegraded"
+assert_contains "$demo_rules" 'cluster=~"ggg|khb|ljw|nmg|oje"'
+assert_contains "$demo_rules" 'deployment="scalecart-worker"'
+assert_contains "$demo_rules" "for: 5s"
+assert_contains "$demo_rules" 'demo: "true"'
+assert_contains "$demo_rules" "https://grafana.teleport.imcherry5778.xyz:3080"
 
 assert_contains "$homepage_services" "Alertmanager:"
 assert_contains "$homepage_services" "https://alertmanager.{{HOMEPAGE_VAR_BASE_DOMAIN}}"
